bd5cf9ee7ff616dbcf84d810b7bc51ac86252560
iamxpf <iamxpf@126.com>
2020-06-29 13:24:59 +0800

    init

---
+++ backend/dubbo-tool/src/main/java/com/xpf/dubbo/dubbotool/DubboToolApplication.java
@@ -0,0 +1,13 @@
+package com.xpf.dubbo.dubbotool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboToolApplication.class, args);
+    }
+
+}
---
+++ backend/dubbo-tool/src/test/java/com/xpf/dubbo/dubbotool/DubboToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpf.dubbo.dubbotool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
